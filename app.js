84c4d47f449a366dddcde8cbe788c4d0d104922e
Ashish Dawan <ashishdawan95@gmail.com>
2026-08-26 00:57:18 +0530

    funcational component

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{kay:1},"Namaste React");
+//React element
+const heading = (<h1 className="head">Namaste React by jsx by ash
+   </h1>);
+//React functional component
+const HeadingComponent =() => {
+    return <h1 className="head">namste React functional component </h1>
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
