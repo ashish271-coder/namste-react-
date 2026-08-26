963dc3e217c94b7790447e0462eec9556a0ea654
Ashish Dawan <ashishdawan95@gmail.com>
2026-08-27 01:30:59 +0530

    component

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React element
-const heading = (<h1 className="head">Namaste React by jsx by ash
-   </h1>);
+const Title =() =>( <h1 className="head">Namaste React by jsx by ash
+   </h1> )
+   const num = 1000;
 //React functional component
-const HeadingComponent =() => {
-    return <h1 className="head">namste React functional component </h1>
-};
+const HeadingComponent =() => (
+     <div id="container"> 
+     <h2>{num}</h2>
+     <h1 className="head">namste React functional component </h1> </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
