06128d67d2877e57c57feac4a2baa02fc241dec1
Ashish Dawan <ashishdawan95@gmail.com>
2026-09-09 00:57:10 +0530

    card

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const Header = () => {
    return (
       <div className="header">
          <div className="logo">
-          <img
-  src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"  alt="Logo"
-/>
+            <img
+               src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" alt="Logo"
+            />
          </div>
          <div className="nav-items">
             <ul>
@@ -22,19 +22,31 @@ const Header = () => {
       </div>
    )
 }
-const RestaurantCard = () => {
-   return(
-      <div className="restaurant-card">
-         <h3>chotiwala</h3>
+const RestaurantCard = (props) => {
+   return (
+      <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
+         <img className="res-logo"
+            alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a2js6idtqyopap5c1kbz"></img>
+         <h3>{props.resname}</h3>
+         <h4>{props.cousine}</h4>
+         <h4>5.5star</h4>
+         <h4>38 minuits</h4>
       </div>
    )
 }
-const Body =() => {
+const Body = () => {
    return (
       <div className="body">
          <div className="search"> search</div>
-         <div className="restaurant-container"></div>
-         <RestaurantCard />
+         <div className="restaurant-container">
+            <RestaurantCard resname="chotiwala"
+            cousine="brackfast lunch dinner"/>
+         <RestaurantCard 
+         resname="kfc"
+         cousine="burger pizza drinks" />
+      
+         </div>
+         
       </div>
    )
 }
