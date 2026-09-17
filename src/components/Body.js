ccf103aa8317d99995a7ab61d1aed764be6439d6
Ashish Dawan <ashishdawan95@gmail.com>
2026-09-18 01:04:54 +0530

    hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,11 @@ import reslist from "../utils/mockdata";
 const Body = () => {
       return (
          <div className="body">
-            <div className="search"> search</div>
+            <div className="filter">
+               <button className="filter-btn" onClick={
+                  () =>{console.log("top resto is ")}}
+                  >Top Rated Restaurant</button>
+            </div>
             <div className="restaurant-container">
             {
                reslist.map((resturnt) =>(<RestaurantCard key ={resturnt.info.id} resData ={resturnt}/>))
